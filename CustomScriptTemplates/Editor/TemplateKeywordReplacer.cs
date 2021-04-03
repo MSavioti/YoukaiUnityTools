dd2da0876b6ec931c16dc58a123ca7c136c13ce7
MSavioti <angelosavioti@gmail.com>
2021-04-02 23:43:55 -0300

    added new monobehaviour template

---
+++ CustomScriptTemplates/Editor/TemplateKeywordReplacer.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using System.IO;
+
+// Author: Alexey Demonov.
+public class ScriptTemplateKeywordReplacer : UnityEditor.AssetModificationProcessor
+{
+    //If there would be more than one keyword to replace, add a Dictionary
+
+    public static void OnWillCreateAsset(string metaFilePath)
+    {
+        string fileName = Path.GetFileNameWithoutExtension(metaFilePath);
+
+        if (!fileName.EndsWith(".cs"))
+            return;
+
+        string actualFilePath = $"{Path.GetDirectoryName(metaFilePath)}{Path.DirectorySeparatorChar}{fileName}";
+        string content = File.ReadAllText(actualFilePath);
+        string newcontent = content.Replace("#PROJECTNAME#", PlayerSettings.productName);
+
+        if (content != newcontent)
+        {
+            File.WriteAllText(actualFilePath, newcontent);
+            AssetDatabase.Refresh();
+        }
+    }
+}
